fbeca93a2728574360004fecafe6e54a8c64bd0a
kajal1shaw <kajalshaw.15.08.2003.2gmail.com>
2023-11-04 17:34:09 +0530

    Django

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,7 @@
+#!C:\Users\kajal\djangogirls\myvenv\Scripts\python.exe
+# When the django-admin.py deprecation ends, remove this script.
+from django.contrib import admin
+from .models import Post
+
+
+admin.site.register(Post)
